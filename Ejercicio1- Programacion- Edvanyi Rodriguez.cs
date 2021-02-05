8085ac918d638d46444a0fb41eb2aab30b02fe1c
Edvanyi <78622281+Edvanyi@users.noreply.github.com>
2021-02-05 17:50:59 -0400

    Add files via upload

---
+++ Ejercicio1- Programacion- Edvanyi Rodriguez.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Programacion_Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int a = 2;
+			int b = 4;
+			int c = 1;
+
+			//numero a producir aacb
+			Console.WriteLine("aacb");
+			Console.Write(a);
+			Console.Write(a);
+			Console.Write(c);
+			Console.WriteLine(b);
+
+			Console.WriteLine("....");
+
+			//numeros a producir bca
+			Console.WriteLine("bca");
+			Console.Write(b);
+			Console.Write(c);
+			Console.WriteLine(a);
+
+			Console.WriteLine("....");
+
+			//numero a producir bcbc
+			Console.WriteLine("bcbc");
+			Console.Write(b);
+			Console.Write(c);
+			Console.Write(b);
+			Console.WriteLine(c);
+
+			//Fin del codigo
+		}
+	}
+}
